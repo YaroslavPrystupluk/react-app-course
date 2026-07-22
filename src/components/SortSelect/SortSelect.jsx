f08dbfa4a3c4da1bf6094fbf3c169b08e6021a72
Yaroslav Prystuplyuk <tyucha_05@ukr.net>
2026-07-22 22:40:44 +0300

    sort logic

---
+++ src/components/SortSelect/SortSelect.jsx
@@ -0,0 +1,16 @@
+import s from "./index.module.css";
+
+const SortSelect = ({ value, onChange }) => {
+  return (
+    <select className={s.select} value={value} onChange={onChange}>
+      <option value="">Sort by</option>
+      <hr />
+      <option value="_sort=level">Level ASC</option>
+      <option value="_sort=-level">Level DESC</option>
+      <option value="_sort=completed">Completed ASC</option>
+      <option value="_sort=-completed">Completed DESC</option>
+    </select>
+  );
+};
+
+export default SortSelect;
--- src/page/HomePage/HomePage.jsx
+++ src/page/HomePage/HomePage.jsx
@@ -5,15 +5,17 @@ import { useFetch } from "../../hooks/useFetch.jsx";
 import { QuestionsCardList } from "../../components/QuestionsCardList";
 import { Loader } from "../../components/Loader";
 import { SearchInput } from "../../components/SearchInput";
+import { SortSelect } from "../../components/SortSelect";
 
 import s from "./index.module.css";
 
 const HomePage = () => {
   const [questions, setQuestions] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [sortSelectValue, setSortSelectValue] = useState("");
 
   const [geQquestions, isLoading, error] = useFetch(async (url) => {
-    const response = await fetch(`${API_URL}/${url}`);
+    const response = await fetch(`${API_URL}/${url}?${sortSelectValue}`);
     if (!response.ok) {
       throw new Error("Something went wrong");
     }
@@ -24,8 +26,8 @@ const HomePage = () => {
   });
 
   useEffect(() => {
-    geQquestions("react");
-  }, []);
+    geQquestions(`react?${sortSelectValue}&question:contains=${searchValue}`);
+  }, [sortSelectValue, searchValue]);
 
   const cards = useMemo(() => {
     return questions.filter((card) =>
@@ -38,17 +40,26 @@ const HomePage = () => {
     setSearchValue(searchValue);
   };
 
+  const onSortSelectChangeHandler = (e) => {
+    const searchValue = e.target.value;
+    setSortSelectValue(searchValue);
+  };
+
   return (
     <>
       <div className={s.controlContainer}>
         <SearchInput value={searchValue} onChange={onSearchChangeHandler} />
+        <SortSelect
+          value={sortSelectValue}
+          onChange={onSortSelectChangeHandler}
+        />
       </div>
       {isLoading && <Loader />}
       {error && <p>Error: {error}</p>}
       {!isLoading && cards.length === 0 && (
         <p className={s.noCardsInfo}>No cards...</p>
       )}
-      <QuestionsCardList cards={cards} />
+      <QuestionsCardList cards={questions} />
     </>
   );
 };
