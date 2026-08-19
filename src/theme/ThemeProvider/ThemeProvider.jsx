ea723bf5daabbc9bd69b1941922d37b26219d89c
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2026-08-19 10:52:42 +0300

    dark theme

--- src/theme/ThemeProvider/ThemeProvider.jsx
+++ src/theme/ThemeProvider/ThemeProvider.jsx
@@ -3,7 +3,8 @@ import { ThemeContext } from "./ThemeContext";
 import {THEME_LOCAL_STORAGE} from "../../constants";
 
 export const ThemeProvider = ({ children }) => {
-  const currentTheme = localStorage.getItem(THEME_LOCAL_STORAGE)  || "light";
+  const currentTheme = localStorage.getItem(THEME_LOCAL_STORAGE) || "light" ;
+  console.log(currentTheme)
   const [theme, setTheme] = useState(currentTheme);
   return <ThemeContext value={{ theme, setTheme }}>{children}</ThemeContext>;
 };
