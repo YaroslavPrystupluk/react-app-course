07e7dff96987dd079a928f2fc2a6b57c76deef16
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2026-08-19 14:15:59 +0300

    dark theme

--- src/theme/ThemeProvider/ThemeProvider.jsx
+++ src/theme/ThemeProvider/ThemeProvider.jsx
@@ -1,10 +1,31 @@
-import { useState } from "react";
+import {useLayoutEffect, useState} from "react";
 import { ThemeContext } from "./ThemeContext";
 import {THEME_LOCAL_STORAGE} from "../../constants";
 
 export const ThemeProvider = ({ children }) => {
   const currentTheme = localStorage.getItem(THEME_LOCAL_STORAGE) || "light" ;
-  console.log(currentTheme)
   const [theme, setTheme] = useState(currentTheme);
-  return <ThemeContext value={{ theme, setTheme }}>{children}</ThemeContext>;
+  useLayoutEffect(() => {
+    const detectTheme = () => {
+      const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+      if (isDark) {
+        setTheme("dark");
+        document.body.classList.remove("darkLayout");
+      } else {
+        currentTheme === "dark" && document.body.classList.add("darkLayout");
+        setTheme(currentTheme);
+      }
+    };
+    detectTheme();
+
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    mediaQuery.addEventListener("change", detectTheme);
+
+    return () => mediaQuery.removeEventListener("change", detectTheme);
+
+  }, []);
+
+
+   return <ThemeContext value={{ theme, setTheme }}>{children}</ThemeContext>;
 };
