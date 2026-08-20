6c0e4cdf1040bcc8bbf2338356482b026e72ef01
Yaroslav Prystupliuk <tyucha_05@ukr.net>
2026-08-20 08:38:49 +0300

    add env

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 export const DEFAULT_PER_PAGE = 10;
-export const API_URL = "http://localhost:8080";
+export const API_URL = import.meta.env.VITE_SERVER_URL;
 export const IS_AUTH_LOCAL_STORAGE = "isAuth";
 export const THEME_LOCAL_STORAGE = "theme";
