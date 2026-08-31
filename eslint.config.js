2327eb6f406f517e5032775902cc8cd9ac87ac35
Yaroslav Prystuplyuk <tyucha_05@ukr.net>
2026-08-31 23:06:42 +0300

    add ts

--- eslint.config.js
+++ eslint.config.js
@@ -2,21 +2,40 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import prettierPlugin from "eslint-plugin-prettier";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   globalIgnores(["dist"]),
   {
-    files: ["**/*.{js,jsx}"],
-    extends: [js.configs.recommended, reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    plugins: {
+      prettier: prettierPlugin,
+    },
     languageOptions: {
       globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
     },
+
     rules: {
-      "prefer-const": "error",
-      "no-console": ["warn", { allow: ["warn", "error"] }],
-      semi: ["error", "always"],
+      "no-console": "warn",
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-expressions": "warn",
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: false,
+          printWidth: 130,
+          tabWidth: 2,
+        },
+      ],
     },
   },
 ]);
